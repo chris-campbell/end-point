98f40aed0438614a50c1628ac9d832599dcf0e29
chris campbell <hopprdev@gmail.com>
2022-08-15 21:04:46 -0400

    Restyle input style

--- client/src/pages/dashboard/components/searchForm/SearchForm.js
+++ client/src/pages/dashboard/components/searchForm/SearchForm.js
@@ -1,15 +1,20 @@
 import React from "react";
 import SuggestionInput from "../suggestionInput/SuggestionInput";
+import styled from "styled-components";
+
+const SearchFormContainer = styled.form`
+  padding: 1rem 0 3rem 0;
+`;
 
 const SearchForm = ({ input, searchTerm, getSearchTerm }) => {
   return (
-    <form>
+    <SearchFormContainer>
       <SuggestionInput
         inputElement={input}
         searchTerm={searchTerm}
         getSearchTerm={getSearchTerm}
       />
-    </form>
+    </SearchFormContainer>
   );
 };
 
--- client/src/pages/dashboard/components/suggestionInput/SuggestionInput.js
+++ client/src/pages/dashboard/components/suggestionInput/SuggestionInput.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./css/SuggestionInput.css";
+import styled from "styled-components";
+
+const SuggestionInputContainer = styled.input`
+  height: 3.5rem;
+  width: 100%;
+  border-radius: 2rem;
+  padding-left: 1.5rem;
+  font-size: 1.2rem;
+  border: 2px solid var(--primary-color-red);
+`;
 
 function SuggestionInput({ inputElement, searchTerm, getSearchTerm }) {
   return (
-    <input
-      id="sender-suggestion-input"
+    <SuggestionInputContainer
       ref={inputElement}
       value={searchTerm}
       placeholder="Type a name..."
