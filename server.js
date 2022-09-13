3d32678e6d5031ef225dd0f42e4333d358188ff7
chris campbell <hopprdev@gmail.com>
2022-09-13 17:34:48 -0400

    Add initial production config

--- server.js
+++ server.js
@@ -115,12 +115,12 @@ const User = require("./src/models/userModel.js");
 //-----------------Deployment--------------------------
 
 const __dirname1 = path.resolve();
-
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "client/build")));
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running successfully");
+  //Set Staticfolder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
